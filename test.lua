80d0a56b58fdcb803a0ff4205fea4492ea6361e9
cjw19941206 <853541718@qq.com>
2022-04-05 22:04:51 +0800

    Changes

---
+++ test.lua
@@ -0,0 +1,5 @@
+package.cpath = package.cpath .. ';C:/Users/85354/AppData/Roaming/JetBrains/IdeaIC2021.3/plugins/EmmyLua/debugger/emmy/windows/x64/?.dll'
+local dbg = require('emmy_core')
+dbg.tcpListen('localhost', 9966)
+
+print("hello")
